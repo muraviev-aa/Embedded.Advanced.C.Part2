fad0c46ef37ef36777322ff943aff6467f4db61f
Alex Muraviev <muraviev-aa@yandex.ru>
2024-07-16 14:40:22 +0300

    Added changes to 3.с

--- hw1/3.c
+++ hw1/3.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+uint32_t cyclic_shift(uint32_t a, uint8_t b)
+{
+    return ((a >> b) | (a << (32 - b)));
+}
+
 int main(void)
 {
     uint32_t a;
     uint8_t b;
     scanf("%u%hhd", &a, &b);
-    printf("%u\n", (a >> b) | (a << (32 - b)));
+    printf("%u\n", cyclic_shift(a, b));
     return 0;
 }
